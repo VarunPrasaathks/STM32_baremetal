806388c3932674530c82347574382b486d94af1d
Varunprasaathks <varunprasaathks07@gmail.com>
2025-11-30 14:29:36 +0530

    tried with tim2

--- timer_using_interrupt_bm/Src/main.c
+++ timer_using_interrupt_bm/Src/main.c
@@ -6,24 +6,24 @@
 
 //base addresses
 #define RCC_BASE 0x40021000UL
-#define TIM6_BASE 0x40001000UL
+#define TIM2_BASE 0x40000000UL
 #define GPIOC_BASE 0x40011000UL
-//#define NVIC_ISER0 0xE000E100UL //this is from cortex M3 architecture Nested Vector Interrupt Controller_Interrrupt Set Enable Register. address taken from m3 ref manual
-#define NVIC_ISER1 (*(volatile uint32_t*)0xE000E104)
+#define NVIC_ISER0 (*(volatile uint32_t*) 0xE000E100UL) //this is from cortex M3 architecture Nested Vector Interrupt Controller_Interrrupt Set Enable Register. address taken from m3 ref manual
+//#define NVIC_ISER1 (*(volatile uint32_t*)0xE000E104)
 
 
 //used register addresses
 #define APB1_ENR            REG32((RCC_BASE + 0x1CUL))  //for TIM6
 #define APB2_ENR            REG32((RCC_BASE + 0x18UL))  //for port c
 
-#define TIM6_CR1            REG32((TIM6_BASE + 0x00UL))
-#define TIM6_CR2            REG32((TIM6_BASE + 0x04UL))
-#define TIM6_DIER           REG32((TIM6_BASE + 0x0CUL))
-#define TIM6_SR             REG32((TIM6_BASE + 0x10UL))
-#define TIM6_EGR            REG32((TIM6_BASE + 0x14UL))
-#define TIM6_CNT            REG32((TIM6_BASE + 0x24UL))
-#define TIM6_PSC            REG32((TIM6_BASE + 0x28UL))
-#define TIM6_ARR            REG32((TIM6_BASE + 0x2CUL))
+#define TIM2_CR1            REG32((TIM2_BASE + 0x00UL))
+#define TIM2_CR2            REG32((TIM2_BASE + 0x04UL))
+#define TIM2_DIER           REG32((TIM2_BASE + 0x0CUL))
+#define TIM2_SR             REG32((TIM2_BASE + 0x10UL))
+#define TIM2_EGR            REG32((TIM2_BASE + 0x14UL))
+#define TIM2_CNT            REG32((TIM2_BASE + 0x24UL))
+#define TIM2_PSC            REG32((TIM2_BASE + 0x28UL))
+#define TIM2_ARR            REG32((TIM2_BASE + 0x2CUL))
 
 
 #define GPIOC_CRH          REG32((GPIOC_BASE + 0x04UL))
@@ -34,7 +34,7 @@ void TIM6_IRQHandler(void);
 int main()
 {
 	//initialise clocks for tim6 and port c
-	APB1_ENR = (1U << 4);
+	APB1_ENR = (1U << 0);
 	APB2_ENR = (1U << 4);
 
 	//set PC13 as LED o/p push pull
@@ -42,17 +42,17 @@ int main()
 	REG32(GPIOC_CRH) |= (0x1U<<20);
 
 	//configure timer
-	TIM6_PSC = 7;
-	TIM6_ARR = 999;
+	TIM2_PSC = 7;
+	TIM2_ARR = 999;
 
-	TIM6_EGR |= 1;
+	TIM2_EGR |= 1;
 
-	TIM6_DIER |= (1U << 0);
+	TIM2_DIER |= (1U << 0);
 
-	//REG32(NVIC_ISER0) |= (1 << 17);
-	NVIC_ISER1 |= (1 << 22);   // enable index at 70 - 16 =54. 54 will be at ISER1. 54-32 = 22
+	NVIC_ISER0 |= (1 << 28);
+	//NVIC_ISER1 |= (1 << 22);   // enable index at 70 - 16 =54. 54 will be at ISER1. 54-32 = 22
 
-	TIM6_CR1 |= (1U << 0);
+	TIM2_CR1 |= (1U << 0);
 
 
 	while (1){
@@ -61,11 +61,11 @@ int main()
 	return 0;
 }
 
-void TIM6_IRQHandler(void){
-	if (TIM6_SR & (1U << 0)) {
+void TIM2_IRQHandler(void){
+	if (TIM2_SR & (1U << 0)) {
 	        // Toggle the LED on PC13
 	        GPIOC_ODR ^= (1U << 13);
 	        // Clear the update interrupt flag (write 0 to the bit)
-	        TIM6_SR &= ~(1U << 0);
+	        TIM2_SR &= ~(1U << 0);
 	    }
 }
